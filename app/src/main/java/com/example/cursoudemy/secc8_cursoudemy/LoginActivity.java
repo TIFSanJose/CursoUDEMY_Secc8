6b466662fe344a79203e1b9c83a74ededc63ef32
cafe <chedecime@gmail.com>
2018-04-25 06:10:24 -0300

    > Se crea la interfaz de login, capturan variables, y crean funciones

--- app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/LoginActivity.java
+++ app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/LoginActivity.java
@@ -2,9 +2,13 @@ package com.example.cursoudemy.secc8_cursoudemy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +33,17 @@ public class LoginActivity extends AppCompatActivity {
         btnSubmit=(Button)findViewById(R.id.btnSubmit);
         etxtPassword=(EditText)findViewById(R.id.etxtPassword);
         etxtEmail=(EditText)findViewById(R.id.etxtEmail);
+    }
+
+    public boolean isValidEmail(String email){
+
+        //TextUtils >> funcion para trabajar con strings
+        //Patterns >> funciones varias, investigar que mas tiene y que diferencias con Pattern (sin "s")
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    public boolean isValidPassword(String password){
+        return password.length() >= 4;
     }
 
 
