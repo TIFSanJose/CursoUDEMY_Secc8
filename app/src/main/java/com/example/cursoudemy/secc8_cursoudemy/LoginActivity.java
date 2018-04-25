03ec52dadafaf3057f48aff0851cb42f96bd7eed
cafe <chedecime@gmail.com>
2018-04-25 07:27:56 -0300

    > Se termina de implementar el login con pasword y email. > Abarco videos 91 al 95. > Se probo en el emulardor, y anduvo bien.

--- app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/LoginActivity.java
+++ app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.example.cursoudemy.secc8_cursoudemy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import java.util.regex.Pattern;
 
@@ -24,6 +27,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        bindUI();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password=etxtPassword.getText().toString();
+                String email=etxtEmail.getText().toString();
+
+                if(login(email,password))
+                    goToMain();
+            }
+        });
     }
 
     //Operaciones y Metodos
@@ -35,6 +51,17 @@ public class LoginActivity extends AppCompatActivity {
         etxtEmail=(EditText)findViewById(R.id.etxtEmail);
     }
 
+    public boolean login(String email, String password){
+        if(!isValidEmail(email)){
+            Toast.makeText(this, "Este email no es valido", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(!isValidPassword(password)){
+            Toast.makeText(this, "Este Password no es valido", Toast.LENGTH_LONG).show();
+            return false;
+        }else
+            return true;
+    }
+
     public boolean isValidEmail(String email){
 
         //TextUtils >> funcion para trabajar con strings
@@ -44,6 +71,18 @@ public class LoginActivity extends AppCompatActivity {
 
     public boolean isValidPassword(String password){
         return password.length() >= 4;
+    }
+
+/**
+ *      ## FLAG ##
+ *      Permiten que en caso que se abra la app y el usuario vuelva atras, la app se cierre. No
+ *      deja sesion abierta.
+ * */
+
+    public void goToMain(){
+        Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 
