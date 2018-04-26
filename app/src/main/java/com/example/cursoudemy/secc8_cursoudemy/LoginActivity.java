de4117efda9a82899ab677579cd2bdac0b5da9e0
cafe <chedecime@gmail.com>
2018-04-26 09:13:59 -0300

    video 54 "Boton Option Menu"
    > Se crea el menu. Se muestra en el navBar.
    > Se gurdan y recuperan las credenciales
    
    > Repasar lo del menu, muy importante.
    > Repasar los de las sesiones, como guardar.

--- app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/LoginActivity.java
+++ app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.example.cursoudemy.secc8_cursoudemy;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +21,8 @@ import java.util.regex.Pattern;
 public class LoginActivity extends AppCompatActivity {
 
     //Atributos de la clase LoginActivity
+
+    private SharedPreferences prefs;
 
     private Switch btnSwitch;
     private Button btnSubmit;
@@ -30,28 +37,35 @@ public class LoginActivity extends AppCompatActivity {
 
         bindUI();
 
+        prefs=getSharedPreferences("Preference", Context.MODE_PRIVATE);
+
+        setCredentialsIfExist();
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String password=etxtPassword.getText().toString();
                 String email=etxtEmail.getText().toString();
 
-                if(login(email,password))
+                if(login(email,password)){
                     goToMain();
+                    saveOfPreference(email, password);
+                }
+
             }
         });
     }
 
     //Operaciones y Metodos
 
-    public void bindUI(){
-        btnSubmit=(Switch)findViewById(R.id.btnSwitch);
+    private void bindUI(){
+        btnSwitch=(Switch)findViewById(R.id.btnSwitch);
         btnSubmit=(Button)findViewById(R.id.btnSubmit);
         etxtPassword=(EditText)findViewById(R.id.etxtPassword);
         etxtEmail=(EditText)findViewById(R.id.etxtEmail);
     }
 
-    public boolean login(String email, String password){
+    private boolean login(String email, String password){
         if(!isValidEmail(email)){
             Toast.makeText(this, "Este email no es valido", Toast.LENGTH_LONG).show();
             return false;
@@ -62,14 +76,14 @@ public class LoginActivity extends AppCompatActivity {
             return true;
     }
 
-    public boolean isValidEmail(String email){
+    private boolean isValidEmail(String email){
 
         //TextUtils >> funcion para trabajar con strings
         //Patterns >> funciones varias, investigar que mas tiene y que diferencias con Pattern (sin "s")
         return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    public boolean isValidPassword(String password){
+    private boolean isValidPassword(String password){
         return password.length() >= 4;
     }
 
@@ -79,11 +93,37 @@ public class LoginActivity extends AppCompatActivity {
  *      deja sesion abierta.
  * */
 
-    public void goToMain(){
+    private void goToMain(){
         Intent intent=new Intent(LoginActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
+    private void saveOfPreference(String email, String password){
+        if(btnSwitch.isChecked()){
+            SharedPreferences.Editor editor=prefs.edit();
+            editor.putString("email", email);
+            editor.putString("password", password);
+            editor.apply();
+        }
+    }
+
+    private void setCredentialsIfExist(){
+        String email=getUserMailPrefs();
+        String password=getUserPassword();
+
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            etxtEmail.setText(email);
+            etxtPassword.setText(password);
+        }
+    }
+
+    private String getUserMailPrefs(){
+        return prefs.getString("email", "");
+    }
+
+    private String getUserPassword(){
+        return prefs.getString("password", "");
+    }
 
 }
--- app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/MainActivity.java
+++ app/src/main/java/com/example/cursoudemy/secc8_cursoudemy/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.cursoudemy.secc8_cursoudemy;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        prefs=getSharedPreferences("Preference", Context.MODE_PRIVATE);
     }
+
+    /*
+     *   Operaciones de Menu
+     *   El Menu se crea en el directorio res.
+     *   Dentro de res, creo un directorio "Android Resources Directory", de tipo "menu", con nombre menu
+     *   En el nuevo directorio menu (res-->menu), creo un archivo "Menu Resources File", de nombre menu.
+     *
+     * */
+
+    /*
+     *
+     *       Esta operacion crea el Menu.
+     *       Lo infla, llena con datos.
+     *
+     * */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.btn_action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.btnActnBar_logout:
+                logOut();
+                return true;
+            case R.id.btnActnBar_forguet_logout:
+                removeSharedPreference();
+                logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logOut(){
+        Intent intent=new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void removeSharedPreference(){
+        prefs.edit().clear().apply();
+    }
+
+
 }
